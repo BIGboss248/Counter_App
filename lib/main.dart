44c1635e7a2da2bf33ac7942176c2af70ad9e501
BIGboss248 <aminjamali79@gmail.com>
2025-12-26 18:35:54 +0330

    dependencies update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:counter_app/counter_observer.dart';
 import 'package:counter_app/utils/logger.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  Bloc.observer = const CounterObserver();
   await initializeLogger();
   runApp(const MyApp());
 }
